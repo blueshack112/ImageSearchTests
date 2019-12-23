b60b7e1e0dbef3e8787c2db6da2f197da62140bc
blueshack112 <ahmed.hassan.112.ha@gmail.com>
2019-12-23 14:38:38 +0500

    Completed the annotation script

--- anotateEditedData.py
+++ anotateEditedData.py
@@ -78,10 +78,14 @@ for index in imagesData:
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
     # New x and y points
-    horxes = []
-    horyes = []
-    verxes = []
-    veryes = []
+    horxes1 = []
+    horyes1 = []
+    horxes2 = []
+    horyes2 = []
+    verxes1 = []
+    veryes1 = []
+    verxes2 = []
+    veryes2 = []
 
     # clear the old list and create template
     imageData[regions].clear()
@@ -90,69 +94,69 @@ for index in imagesData:
     time_to_run = current_milli_time()
 
     """ Getting horizontal points (left to right) """
+    firstHorizontalLefttoRightColumn = None
     for row in range (0, image.shape[0], 10):
         # For every row in the array
         for column in range(0, image.shape[1]):
             # If a pixel is found that is not white (255), record that x and y value
             # And break the column loop
             if not image[row][column] == 255:
-                horxes.append(column)
-                horyes.append(row)
-                lastHorizontalLefttoRightColumn = column
-                lastHorizontalLefttoRightRow = row
+                horxes1.append(column)
+                horyes1.append(row)
+                if not firstHorizontalLefttoRightColumn:
+                    firstHorizontalLefttoRightColumn = column
+                else:
+                    lastHorizontalLefttoRightColumn = column
+                break
+
+    """ Getting horizontal points (left to right) """
+    firstHorizontalRighttoLeftColumn = None
+    for row in range (image.shape[0]-1 , 0 , -10):
+        for column in range (image.shape[1]-1, 0, -1):
+            if not image[row][column] == 255:
+                horxes2.append(column)
+                horyes2.append(row)
+                if not firstHorizontalRighttoLeftColumn:
+                    firstHorizontalRighttoLeftColumn = column
+                else:
+                    lastHorizontalRighttoLeftColumn = column
                 break
     
-    
     """ Getting vertical points (down to up) """
-    for column in range (0, image.shape[1], 10):
+    for column in range (lastHorizontalLefttoRightColumn, firstHorizontalRighttoLeftColumn, 10):
         # For every row in the array
         for row in range(image.shape[0]-1, 0, -1):
             # If a pixel is found that is not white (255), record that x and y value
             # And break the column loop
             if not image[row][column] == 255:
-                verxes.append(column)
-                veryes.append(row)
+                verxes1.append(column)
+                veryes1.append(row)
                 break
-
-    """ Getting horizontal points (left to right) """
-    for row in range (image.shape[0]-1 , 0 , -10):
-        for column in range (image.shape[1]-1, 0, -1):
-            if not image[row][column] == 255:
-                horxes.append(column)
-                horyes.append(row)
-                break
-    
-    # DEBUG
-    newRegion.clear()
-    oneregion = {}
-    oneregion['region_attributes'] = {"teeshirt": "1"}
-    oneshapeattribute = {}
-    oneshapeattribute["name"] = "polygon"
-    oneshapeattribute["all_points_x"] = horxes
-    oneshapeattribute["all_points_y"] = horyes
-    oneregion[shape_attributes] = oneshapeattribute
-    newRegion.append(oneregion)
-    
 
     """ Getting vertical points (up to down) """
-    for column in range (image.shape[1]-1, 0, -10):
+    for column in range (lastHorizontalRighttoLeftColumn, firstHorizontalLefttoRightColumn, -10):
         # For every row in the array
         for row in range(0, image.shape[0]):
             # If a pixel is found that is not white (255), record that x and y value
             # And break the column loop
             if not image[row][column] == 255:
-                verxes.append(column)
-                veryes.append(row)
+                verxes2.append(column)
+                veryes2.append(row)
                 break
-    # DEBUG
-    tworegion = {}
-    tworegion['region_attributes'] = {"teeshirt": "1"}
-    twoshapeattribute = {}
-    twoshapeattribute["name"] = "polygon"
-    twoshapeattribute["all_points_x"] = verxes
-    twoshapeattribute["all_points_y"] = veryes
-    tworegion[shape_attributes] = twoshapeattribute
-    newRegion.append(tworegion)
+
+    # Finalizing all xes and yes
+    finalX = horxes1 + verxes1 + horxes2 + verxes2
+    finalY = horyes1 + veryes1 + horyes2 + veryes2
+    
+    # Creating the region and saving it in the json element
+    fiveregion = {}
+    fiveregion['region_attributes'] = {"teeshirt": "1"}
+    fiveshapeattribute = {}
+    fiveshapeattribute["name"] = "polyline"
+    fiveshapeattribute["all_points_x"] = finalX
+    fiveshapeattribute["all_points_y"] = finalY
+    fiveregion[shape_attributes] = fiveshapeattribute
+    newRegion.append(fiveregion)
 
     imageData[regions] = newRegion
 
